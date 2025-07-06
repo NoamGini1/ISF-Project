b498aed0b0162c044322bd6a4646551dec3a0ec1
Noam Gini <noamg@moveo.co.il>
2025-07-06 15:12:35 +0300

    adding id to the join us section

--- front-page.php
+++ front-page.php
@@ -6,16 +6,19 @@ get_header();
 while (have_posts()) : the_post();
     the_content();
 endwhile;
-
-
-$join_us_form_id = get_field('join_us_form_id', 'option');
-if ($join_us_form_id) {
-    get_template_part('template-parts/form/form-render', null, array(
-        'form_id' => $join_us_form_id,
-        'form_class' => 'custom-form--join-us',
-    ));
-}
 ?>
+<section id="join-us">
+    <?php
+    $join_us_form_id = get_field('join_us_form_id', 'option');
+    if ($join_us_form_id) {
+        get_template_part('template-parts/form/form-render', null, array(
+            'form_id' => $join_us_form_id,
+            'form_class' => 'custom-form--join-us',
+        ));
+    }
+    ?>
+</section>
+
 
 <?php
 get_footer(); ?>
